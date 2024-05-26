b9560781ce7b5df4c37b75b4097b116aaf928f3d
Muneera Shaik <muneerashaik14@gmail.com>
2024-05-26 22:15:04 +0400

    E2e with webdriverIO-Initial commit

---
+++ features/pageobjects/Users.page.ts
@@ -0,0 +1,72 @@
+import { APICalls } from "../enums/APICalls";
+import { addLog, click, selectVisibleText, setText } from "../utils/commands";
+import Page from "./page";
+
+class UsersPage extends Page {
+
+    private get url_textbox() { return $('#urlvalue'); }
+    private get ajax_btn() { return $('#submitajax'); }
+    private get success_elem() { return $('.alert-success'); }
+    private get status_text() { return $('#statuspre'); }
+    private get output_area() { return $('#outputpre'); }
+
+    private get method_dropdown() { return $('#httpmethod') }
+    private get addParam_Btn() { return $('#addprambutton') }
+    
+    private get paramName1_textbox() { 
+        return $("//div[@id='allparameters']//input[contains(@class,'fakeinputname') and @value]") 
+    }
+    private get paramValue1_textbox() { 
+        return $("//div[@id='allparameters']//input[contains(@class,'realinputvalue') and @value]") 
+    }
+    private get paramName2_textbox() { 
+        return $("(//div[@id='allparameters']//input[contains(@class,'fakeinputname') and @value])[2]") 
+    }
+    private get paramValue2_textbox() { 
+        return $("(//div[@id='allparameters']//input[contains(@class,'realinputvalue') and @value])[2]") 
+    }
+
+    async openApp(pageurl: string) {
+        await browser.url(pageurl)
+        addLog(`Opening ${pageurl}`)
+        await browser.maximizeWindow();
+        addLog("Maximizing window")
+    }
+    
+    async enterAPIUrl(apiendpoint: string) {
+         await setText(this.url_textbox, apiendpoint)
+    }
+
+    async clickOnAjaxBtn() {
+        await click(this.ajax_btn)
+    }
+
+    async getStatusText(): Promise<string> {
+        await this.success_elem.waitForDisplayed();
+        return this.status_text.getText()
+    }
+
+    async getOutputText(): Promise<string> {
+        return this.output_area.getText()
+    }
+
+    async selectMethod(apiType: APICalls) {
+        await selectVisibleText(this.method_dropdown, apiType)
+    }
+
+    async clickOnAddParamBtn() {
+        await click(this.addParam_Btn)
+    }
+
+    async enterFirstParams(key: string, value:string) {
+        await setText(this.paramName1_textbox, key)
+        await setText(this.paramValue1_textbox, value)
+    }
+
+    async enterSecondParams(key: string, value:string) {
+        await setText(this.paramName2_textbox, key)
+        await setText(this.paramValue2_textbox, value)
+    }
+
+}
+export default new UsersPage()
---
+++ features/step-definitions/E2E_User.ts
@@ -0,0 +1,68 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+import { expect, $ } from '@wdio/globals'
+import { BASE_URI } from '../config/APIConfig';
+import UsersPage from '../pageobjects/Users.page';
+import supertest from 'supertest'
+import { APICalls } from '../enums/APICalls';
+import assertions from '../utils/assertions';
+import { addLog } from '../utils/commands';
+
+const request = supertest(BASE_URI)
+let response: supertest.Response;
+
+const payload = {
+    "name": "sadab",
+    "job": "tester"
+}
+
+Given(/^I am on page (.+)$/, async (pageurl: string) => {
+    await UsersPage.openApp(pageurl);
+});
+
+When(/^I perform (.+) user search$/, async (endpoint: string) => {
+    await UsersPage.selectMethod(APICalls.GET)
+    await UsersPage.enterAPIUrl(BASE_URI + endpoint);
+    await UsersPage.clickOnAjaxBtn();
+})
+
+When(/^I make GET (.+) api call$/, async (endpoint: string) => {
+    response = await request.get(endpoint)
+    addLog(`GET api call to endpoint: ${endpoint}`)
+})
+
+Then(/^I validate the search result$/, async () => {
+    const ui_status = await UsersPage.getStatusText();
+    const ui_response = JSON.parse(await UsersPage.getOutputText());
+
+    assertions.toContain(ui_status, response.statusCode.toString())
+    assertions.toEqual(JSON.stringify(ui_response), JSON.stringify(response.body))
+    assertions.toEqual(ui_response.data.email, response.body.data.email)
+})
+
+
+When(/^I perform create use search for api (.+)$/, async (service: string) => {
+    await UsersPage.selectMethod(APICalls.POST)
+    await UsersPage.enterAPIUrl(BASE_URI + service);
+    await UsersPage.clickOnAddParamBtn()
+    await UsersPage.enterFirstParams("name", payload.name)
+    await UsersPage.clickOnAddParamBtn()
+    await UsersPage.enterSecondParams("job", payload.job)
+    await UsersPage.clickOnAjaxBtn();
+})
+
+When(/^I make POST (.+) api call$/, async (endpoint: string) => {
+    response = await request
+        .post(endpoint)
+        .send(payload)
+        .set('content-type', 'application/json');
+    addLog(`POST api call to endpoint: ${endpoint}`)
+})
+
+Then(/^I validate the create user search result$/, async () => {
+    const ui_status = await UsersPage.getStatusText();
+    const ui_response = JSON.parse(await UsersPage.getOutputText());
+
+    assertions.toContain(ui_status, response.statusCode.toString())
+    assertions.toEqual(ui_response.name, response.body.name)
+    assertions.toEqual(ui_response.job, response.body.job)
+})
